a50550ae0c19defce033610a783faaf08a6d0d99
Ari <mariia.barabukha@gmail.com>
2023-03-28 12:46:08 +0300

    fix code style according to linter

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const http = require('http');
+const http = require('http')
 
-// const hostname = '127.0.0.1';
-const port = 8080;
+const port = 8080
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World')
+})
 
 server.listen(port, () => {
-  console.log(`Server running`);
-});
+  console.log('Server running')
+})
